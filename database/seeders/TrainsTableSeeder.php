abb875b70606f15f20a5e5113d2ec5ce71d2fbd8
deca-francesco <francescodecarlo70@gmail.com>
2025-03-12 12:49:23 +0100

    ADD: seeder new records

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// importo il faker e gli assegno un alias e lo passo alla funzione run()
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        // uso il faker
+        for ($i = 0; $i < 20; $i++) {
+            // istanza della classe
+            $newTrain = new Train();
+
+            // popolo i campi
+            $newTrain->company = $faker->company();
+            $newTrain->train_code = $faker->numberBetween(1000, 9999);
+            $newTrain->carriages = $faker->numberBetween(4, 10);
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->on_time = $faker->boolean();
+            $newTrain->cancelled = $faker->boolean();
+
+            // salvo il record
+            $newTrain->save();
+        }
+    }
+}
